e288cabbbf722358d851aef5a9fd5e27247f1af6
Tony Lykke <tony@roganartu.com>
2021-07-03 21:46:56 -0400

    use right rust-analyzer binary

--- rust-analyzer/update.sh
+++ rust-analyzer/update.sh
@@ -6,10 +6,10 @@ _target=$1
 echo "Updating ${_target} from GitHub releases"
 
 _url="$(curl --silent "https://api.github.com/repos/rust-analyzer/${_target}/releases/latest" \
-  | jq -r '.assets[] | select(.name | endswith('\"'-linux-musl.gz'\"')).browser_download_url')"
+  | jq -r '.assets[] | select(.name | endswith('\"'x86_64-unknown-linux-gnu.gz'\"')).browser_download_url')"
 
 if [ -z "${_url}" ]; then \
-  printf "\e[38;5;196m\nUnable to determine download link for ${_target} linux-musl.gz\n\n\e[0m"; \
+  printf "\e[38;5;196m\nUnable to determine download link for ${_target} x86_64-unknown-linux-gnu.gz\n\n\e[0m"; \
   exit 1; \
 fi
 
@@ -17,9 +17,9 @@ _olddir="$(pwd)"
 
 cd /tmp
 wget "${_url}"
-gunzip -f rust-analyzer*-linux-musl.gz
-mv "/tmp/rust-analyzer"*"-linux-musl" "${_olddir}/${_target}/${_target}"
-rm -rf "/tmp/rust-analyzer"*"-linux-musl"*
+gunzip -f rust-analyzer*-linux-gnu.gz
+mv "/tmp/rust-analyzer"*"-linux-gnu" "${_olddir}/${_target}/${_target}"
+rm -rf "/tmp/rust-analyzer"*"-linux-gnu"*
 
 # Make sure it's executable
 chmod +x "${_olddir}/${_target}/"*
