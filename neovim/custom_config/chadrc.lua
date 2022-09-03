5c58f22b5a88f631aef35e737dc0779a745e1064
Tony Lykke <github@roganartu.com>
2022-09-03 17:27:38 -0400

    fix nvchad lspserver configs

--- neovim/custom_config/chadrc.lua
+++ neovim/custom_config/chadrc.lua
@@ -6,29 +6,6 @@ local userPlugins = require "custom.plugins"
 
 M.plugins = {
 
-  lspconfig = {
-    servers = {
-      "ansiblels",
-      "bashls",
-      "ccls",
-      "clangd",
-      "cmake",
-      "cssls",
-      "dockerls",
-      "emmet_ls",
-      "gopls",
-      "html",
-      "jsonls",
-      "pyright",
-      "rust_analyzer",
-      "salt_ls",
-      "sqls",
-      "tailwindcss",
-      "tsserver" ,
-      "yamlls" ,
-    }
-  },
-
   options = {
     statusline = {
       separator_style = "round",
@@ -38,6 +15,7 @@ M.plugins = {
   override = {
     ["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
     ["kyazdani42/nvim-tree.lua"] = pluginConfs.nvimtree,
+    ["williamboman/mason.nvim"] = pluginConfs.mason,
   },
 
   ui = {
--- neovim/custom_config/plugins/configs.lua
+++ neovim/custom_config/plugins/configs.lua
@@ -24,4 +24,67 @@ M.nvimtree = {
    },
 }
 
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev
+    "css-lsp",
+    "html-lsp",
+    "eslint-lsp",
+    "typescript-language-server",
+    "json-lsp",
+
+    -- rust stuff
+    "rust-analyzer",
+
+    -- python things
+    "pyright",
+    "pylint",
+    "jedi-language-server",
+    "black",
+    "isort",
+
+    -- misc
+    "yaml-language-server",
+    "yamllint",
+    "yamlfmt",
+    "prettier",
+    "curlylint",
+    "taplo",
+    "gitlint",
+    "buf",
+    "sql-formatter",
+    "sqlls",
+
+    -- Salt stuff
+    "salt-lsp",
+
+    -- Terraform stuff
+    "terraform-ls",
+    "tflint",
+
+    -- Jenkinsfile stuff
+    "groovy-language-server",
+
+    -- go stuff
+    "gopls",
+    "goimports",
+
+    -- docker stuff
+    "dockerfile-language-server",
+    "hadolint",
+
+    -- ansible stuff
+    "ansible-language-server",
+
+    -- shell
+    "shfmt",
+    "shellcheck",
+    "bash-language-server",
+  },
+}
+
 return M
